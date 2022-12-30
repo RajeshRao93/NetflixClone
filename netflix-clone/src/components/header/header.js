18c90a005afaa6511d3cd91539877ee864acd66a
RajeshRao93 <rajesh.rao0593@gmail.com>
2022-12-30 19:17:57 +0100

    Added header, new content on home page

--- netflix-clone/src/components/header/header.js
+++ netflix-clone/src/components/header/header.js
@@ -1,15 +1,48 @@
-import logo from "../.././logo.svg";
+import React, { useState, useEffect } from "react";
+import { Navbar } from "react-bootstrap";
+import logo from "../../assets/images/netflix-logo.png";
 import "./header.css";
-import "../../App.css";
 
 const Header = () => {
+  const [scrollState, setScrollState] = useState(false);
+  useEffect(() => {
+    //change nav bar color on scroll
+    let listener = document.addEventListener("scroll", (e) => {
+      var scrollLength = document.scrollingElement.scrollTop;
+      if (scrollLength >= 150) {
+        if (!scrollState) setScrollState(true);
+      } else {
+        if (scrollState) setScrollState(false);
+      }
+    });
+    return () => {
+      document.removeEventListener("scroll", listener);
+    };
+  }, [scrollState]);
+
+  const buttonStyle = { borderRadius: "2rem", textTransform: "lowercase" };
+  const navbarStyle = {
+    backgroundColor: !scrollState ? "transparent" : "#005fff80",
+    justifyContent: "center",
+  };
+
   return (
-    <div>
-      <header className="header">
-        <img src={logo} className="header-logo" alt="logo" />
-        <p>Hello world!</p>
-      </header>
-    </div>
+    <Navbar
+      className="navbar-transparent navbar navbar-expand-lg"
+      style={navbarStyle}
+      fixed="top"
+    >
+      <Navbar.Brand href="/Home/">
+        <div className="header-container">
+          <img src={logo} className="header-logo" alt="logo" />
+          <a>Home</a>
+          <a>TV Shows</a>
+          <a>Movies</a>
+          <a>New & Popular</a>
+          <a>My List</a>
+        </div>
+      </Navbar.Brand>
+    </Navbar>
   );
 };
 
